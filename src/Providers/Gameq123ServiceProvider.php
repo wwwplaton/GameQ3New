d64f0478e290190fc11ba1d45af34e9dfc4072e7
wwwplaton <xxxplaton@ya.ru>
2024-05-12 14:11:34 +0300

    fix

---
+++ src/Providers/Gameq123ServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Gameq3\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Gameq3\Console\Commands\ExampleCommand;
+use Gameq3\Console\Commands\MakePackage;
+
+class LaravelGameQ3ServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ExampleCommand::class,
+                
+            ]);
+        }
+
+    }
+}
